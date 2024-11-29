2fa5963fa4ad2db08b2a83884fb48270d150abdb
DavidGarcia0309 <davidgarciauat@gmail.com>
2024-11-29 00:50:41 -0600

    Cambios para merge programador2 a main

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log("Hello Wordl")
 console.log("Cambio 4")
 console.log("cambio de nuevo")
+
 console.log("Rama del programador 2")
-console.log("Haciendo merge del programador 2 al programador 1!");
+console.log("Haciendo merge del programador 2 al main");
+
+
