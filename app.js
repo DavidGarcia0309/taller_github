cfc160c1394f519c5b326c789677924e7a9a040c
DavidGarcia0309 <davidgarciauat@gmail.com>
2024-11-29 01:14:53 -0600

    Commit merge

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log("Cambio 4")
 console.log("cambio de nuevo")
 console.log("ëstoy en la rama del programador 1")
 console.log("otro cambio")
+
+console.log("Cambios para el merge del programador 1 al main")
+console.log("Cambiosssssssss")
