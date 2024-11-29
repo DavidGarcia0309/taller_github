0605507963a2598a8a7bb033ad098f17b55cb216
DavidGarcia0309 <davidgarciauat@gmail.com>
2024-11-29 02:48:31 -0600

    Merge de programador a main

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 console.log("Hello Wordl")
 console.log("Cambio 4")
 console.log("cambio de nuevo")
-console.log("ëstoy en la rama del programador 1")
+console.log("rama del programador 1")
 console.log("otro cambio")
 
 console.log("Cambios para el merge del programador 1 al main")
 console.log("Cambiosssssssss")
+
+console.log("Cambio para hacer el merge de programador 1 a main")
