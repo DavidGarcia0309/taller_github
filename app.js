c4203249799149442f2e08043b4d81013b87f496
DavidGarcia0309 <davidgarciauat@gmail.com>
2024-11-29 00:31:56 -0600

    Estoy haciendo el cambio para hacer el merge

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Hello Wordl")
 console.log("Cambio 4")
 console.log("cambio de nuevo")
 console.log("Rama del programador 2")
+console.log("Haciendo merge del programador 2 al programador 1!");
